aa3583d4a87d219bd12f01b448948b1f8c891af4
AvatarNit <sajjanithik@icloud.com>
2024-06-01 11:03:38 -0400

    Set up database

--- recipes/setup.sql
+++ recipes/setup.sql
@@ -1,20 +1,51 @@
-.open recipes.db
-.mode box
 
--- This will enforce the foreign key relationships
-PRAGMA foreign_keys = ON;
+DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS admins;
+DROP TABLE IF EXISTS history;
+DROP TABLE IF EXISTS ingredients;
 
--- DROP TABLE IF EXISTS owners;
+CREATE TABLE IF NOT EXISTS recipes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    picName TEXT NOT NULL,
+    videoUrl TEXT,
+    ingredients TEXT NOT NULL,
+    instructions TEXT NOT NULL,
+    cookTime INTEGER,
+    servings INTEGER,
+    category TEXT,
+    class TEXT
+);
 
--- CREATE TABLE IF NOT EXISTS owners(
---     owner_id INTEGER,
---     first_name TEXT NOT NULL,
---     last_name TEXT NOT NULL,
---     email TEXT NOT NULL UNIQUE,
---     password TEXT,
---     needs_reset INTEGER DEFAULT 1,
---     PRIMARY KEY (owner_id)
--- );
+CREATE TABLE IF NOT EXISTS admins (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    passwords TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    aName TEXT NOT NULL,
+    action TEXT NOT NULL,
+    recipeNum INTEGER NOT NULL
+);
 
--- CREATE VIEW IF NOT EXISTS dog_owners AS
--- SELECT * FROM dogs NATURAL JOIN owners;
+CREATE TABLE IF NOT EXISTS ingredients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    picLocation TEXT NOT NULL
+);
+
+INSERT INTO recipes (name,picName,videoUrl,ingredients,instructions,cookTime,servings,category,class) VALUES ("Lemon Rice", "/static/food/lemon rice/lemon.heic", "https://youtu.be/DVoTi4mve2U?si=ORpf8WDUCEfjxoXC", "1_Cup:Peanuts,5:Chilies,8:Curry Leaves,1_tsp:Jeera,1_tsp:Chana Dal,1_tsp:Mustard Seeds,1_tsp:Urad Dal,1_tsp:Turmeric,Lemon Extract,Salt,2_Cup:Cooked Rice", "1. Break up the rice.,2. Heat oil in a pan.,3. Fry peanuts until golden.,4. Add Chana Dal, Jeera, Mustard Seeds, and Urad Dal. Mix.,5. Add Curry Leaves and Chilies. Mix.,6. Add turmeric. Mix.,7. Add rice. Mix well.,8. Add lemon juice and salt to taste.,9. Enjoy!",15,2,"Breakfast","Veg");
+INSERT INTO admins (username, passwords) VALUES ("NITHIK", "704090");
+INSERT INTO history (aName, action, recipeNum) VALUES ("NITHIK", "Create",1);
+INSERT INTO ingredients (name,picLocation) VALUES ("Chana Dal", "/static/ingredients/chana dal.png");
+INSERT INTO ingredients (name,picLocation) VALUES ("Chilies", "/static/ingredients/chilies.png");
+INSERT INTO ingredients (name,picLocation) VALUES ("Cooked Rice", "/static/ingredients/cooked rice.png");
+INSERT INTO ingredients (name,picLocation) VALUES ("Curry Leaves", "/static/ingredients/curry leaves.png");
+INSERT INTO ingredients (name,picLocation) VALUES ("Jeera", "/static/ingredients/jeera.png");
+INSERT INTO ingredients (name,picLocation) VALUES ("Lemon Extract", "/static/ingredients/lemon extract.png");
+INSERT INTO ingredients (name,picLocation) VALUES ("Mustard Seeds", "/static/ingredients/mustard seeds.png");
+INSERT INTO ingredients (name,picLocation) VALUES ("Peanuts", "/static/ingredients/peanuts.png");
+INSERT INTO ingredients (name,picLocation) VALUES ("Salt", "/static/ingredients/salt.png");
+INSERT INTO ingredients (name,picLocation) VALUES ("Turmeric", "/static/ingredients/turmeric.png");
+INSERT INTO ingredients (name,picLocation) VALUES ("Urad Dal", "/static/ingredients/urad dal.png");
